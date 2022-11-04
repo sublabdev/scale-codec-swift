92094fb2dd68426568a761dcb6c1535baefddce4
Tigran Iskandaryan <tiskandaryan@griddynamics.com>
2022-11-04 11:42:33 +0300

    Made decoder and encoder public, updated version

--- ScaleCodecSwift/Classes/Coder/ScaleCoder.swift
+++ ScaleCodecSwift/Classes/Coder/ScaleCoder.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 public final class ScaleCoder {
-    let encoder: ScaleEncoder
-    let decoder: ScaleDecoder
+    public let encoder: ScaleEncoder
+    public let decoder: ScaleDecoder
     
     public init(encoder: ScaleEncoder, decoder: ScaleDecoder) {
         self.encoder = encoder
